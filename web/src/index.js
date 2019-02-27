308c53683265cec19b234db7e8189d20374cf912
ukabuer <ukabuer@live.com>
2019-02-27 12:59:47 +0800

    remove web to separate repo

--- web/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './App';
-
-const el = document.getElementById('app');
-ReactDOM.render(<App />, el);
--- web/src/store/config.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Config = React.createContext({
-  server: 'localhost:3000/',
-  update: () => {},
-});
-
-export default Config;
