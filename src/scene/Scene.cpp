b124147fde5df0151d4627e8c8e2f7a250ff271b
ukabuer <ukabuer@live.com>
2020-03-12 14:37:16 +0800

    fix: transform tfcn hex color to rgb float

--- src/scene/Scene.cpp
+++ src/scene/Scene.cpp
@@ -18,6 +18,13 @@ auto Scene::deserialize(const rapidjson::Value &json) -> Scene {
 
   seria::deserialize(scene, json);
 
+  // handle tfcns
+  for (auto &tfcn : scene.tfcns) {
+    for (auto &point: tfcn) {
+      point.color = hex_color_to_float(point.hex_color);
+    }
+  }
+
   // handle datasets
 
   return scene;
