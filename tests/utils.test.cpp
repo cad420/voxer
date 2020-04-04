21449968f4d5c50f16e5209bd48019ae8e8eac2d
ukabuer <ukabuer@live.com>
2020-04-04 23:40:56 +0800

    fix tests

--- tests/utils.test.cpp
+++ tests/utils.test.cpp
@@ -1,5 +1,3 @@
-#include <rapidjson/stringbuffer.h>
-#include <rapidjson/writer.h>
 #include <voxer/Scene.hpp>
 #include <voxer/scene/Camera.hpp>
 #include <voxer/utils.hpp>
@@ -37,38 +35,4 @@ TEST_CASE("hex_color_to_float", "[conversion]") {
   const string v3 = "#00ff00";
   auto res3 = hex_color_to_float(v3);
   REQUIRE((res3[0] == 0.0f && res3[1] == 1.0f && res3[2] == 0.0f));
-}
-
-TEST_CASE("json", "[deser, ser]") {
-  voxer::Camera camera{};
-  camera.width = 100;
-  const voxer::Volume volume{};
-  voxer::Scene scene{};
-  scene.volumes.push_back(volume);
-  scene.camera = camera;
-
-  auto json = formatter::serialize(scene);
-  {
-    rapidjson::StringBuffer buffer{};
-    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
-    json.Accept(writer);
-    cout << buffer.GetString() << endl;
-  }
-
-  auto value = json.GetObject();
-  auto it = value.FindMember("camera");
-  auto &c = it->value;
-  c["width"] = 233;
-
-  formatter::deserialize(scene, json);
-  {
-    rapidjson::StringBuffer buffer{};
-    rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
-    json.Accept(writer);
-    cout << buffer.GetString() << endl;
-  }
-
-  REQUIRE(std::is_class<voxer::SceneDataset>::value);
-  REQUIRE((std::is_class<voxer::SceneDataset>::value &&
-           !formatter::is_vector<voxer::SceneDataset>::value));
 }
