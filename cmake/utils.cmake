ffd1a0ce356255912e74409dbac9bb919c97735c
ukabuer <ukabuer@live.com>
2020-04-20 23:28:33 +0800

    use a cmake function to embed shaders

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,3 +1,5 @@
+include_guard()
+
 if (${CMAKE_VERSION} VERSION_LESS 3.14)
   macro(FetchContent_MakeAvailable NAME)
     FetchContent_GetProperties(${NAME})
@@ -9,8 +11,35 @@ if (${CMAKE_VERSION} VERSION_LESS 3.14)
 endif ()
 option(FETCHCONTENT_QUIET "" OFF)
 
-if (UNIX)
-#  set(CMAKE_CXX_FLAGS "-Wall -Wextra")
-#  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -fsanitize=address -fsanitize=leak -fsanitize=undefined")
-#  set(CMAKE_LINKER_FLAGS_DEBUG "${CMAKE_LINKER_FLAGS_DEBUG} -fsanitize=address -fsanitize=leak -fsanitize=undefined")
-endif ()
+include(CMakeParseArguments)
+function(embed_shaders)
+  cmake_parse_arguments(
+      PARSED_ARGS # prefix of output variables
+      "" # list of names of the boolean arguments (only defined ones will be true)
+      "CONFIG_FILE;TARGET" # list of names of mono-valued arguments
+      "SRCS" # list of names of multi-valued arguments (output variables are lists)
+      ${ARGN} # arguments of the function to parse, here we take the all original ones
+  )
+
+  if (NOT PARSED_ARGS_CONFIG_FILE)
+    message(ERROR "You must provide a config file")
+  endif ()
+
+  if (NOT PARSED_ARGS_TARGET)
+    message(ERROR "You must provide a target file")
+  endif ()
+
+  foreach (shader_file ${PARSED_ARGS_SRCS})
+    get_filename_component(shader_name ${shader_file} NAME)
+    file(READ ${shader_file} ${shader_name})
+    set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS ${shader_file})
+  endforeach ()
+
+  set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS ${PARSED_ARGS_CONFIG_FILE})
+  configure_file(
+      ${PARSED_ARGS_CONFIG_FILE}
+      ${PARSED_ARGS_TARGET}
+      ESCAPE_QUOTES
+      NEWLINE_STYLE LF
+  )
+endfunction()
