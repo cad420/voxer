752be512622b8ff0e3b1e3200a3dd0ede37a4755
ukabuer <ukabuer@live.com>
2020-11-23 12:53:51 +0800

    refactor: use seria v0.1

--- apps/VoxerWorker/third_party/seria.cmake
+++ apps/VoxerWorker/third_party/seria.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 FetchContent_Declare(
     seria
     GIT_REPOSITORY https://github.com/ukabuer/seria.git
-    GIT_TAG 78452629b800088cd1e434529afdc00a704dd6c7
+    GIT_TAG v0.1
     GIT_SHALLOW true
     GIT_PROGRESS TRUE
 )
